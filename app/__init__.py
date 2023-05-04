df74ae5fec7a152b1ebb4c3b7aaa2732575695d5
Diana Akhmedova <90854530+Dianartemis@users.noreply.github.com>
2023-05-04 12:49:21 -0400

    init files

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+import random
+from flask import Flask
+app = Flask(__name__) 
+
+@app.route("/")       
+def hello_world():
+    print("the __name__ of this module is... ")
+    print(__name__)
+    return "No hablo queso!"
