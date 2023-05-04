13142ed9bc8a041d6aec455c7ae50f0ebedb5871
Diana Akhmedova <90854530+Dianartemis@users.noreply.github.com>
2023-05-04 12:50:58 -0400

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -7,3 +7,7 @@ def hello_world():
     print("the __name__ of this module is... ")
     print(__name__)
     return "No hablo queso!"
+
+if __name__ == "__main__":  # true if this file NOT imported
+    app.debug = True        # enable auto-reload upon code change
+    app.run()
