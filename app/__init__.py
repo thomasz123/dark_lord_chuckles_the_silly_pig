7675b16ddc8a02439eb26dada9acfc0fc414dad3
Diana Akhmedova <90854530+Dianartemis@users.noreply.github.com>
2023-05-04 13:01:09 -0400

    edits

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 import random
-from flask import Flask
+from flask import Flask, redirect, render_template, request, session, url_for
 app = Flask(__name__) 
 
 @app.route("/")       
-def hello_world():
-    print("the __name__ of this module is... ")
-    print(__name__)
-    return "No hablo queso!"
+def auth():
+    # print("the __name__ of this module is... ")
+    # print(__name__)
+    return render_template('auth.html')
 
 
 if __name__ == "__main__":  # true if this file NOT imported
