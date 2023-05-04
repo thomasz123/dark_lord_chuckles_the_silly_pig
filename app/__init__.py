83fcd9fe0cb3b6e87dfb4ba1067dfa9fd764891c
Diana Akhmedova <90854530+Dianartemis@users.noreply.github.com>
2023-05-04 13:18:53 -0400

    edits

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,38 @@ def auth():
     # print(__name__)
     return render_template('auth.html')
 
+# @app.route("/login")
+# def login():
+#     if 'username' in session: #if there is a session going on, will direct to home page
+#         return render_template('home.html')
+#     return render_template('login.html')  #if no session, will prompt to login
 
+# @app.route("/register")
+# def register():
+    
+
+# @app.route("/verifylogin", methods = ['GET', 'POST'])
+# def login():
+#     if (table_handler.user_check(request.form["username"]) == True):
+#         if (table_handler.password_check(request.form["username"], request.form["password"]) == True):
+#             session['username'] = request.form['username']
+#             return redirect('/')
+#         else:
+#             flash("Invalid username/password", "danger")
+#             return render_template('login.html')
+#     else: 
+#         flash("Please enter your username/password", "danger")
+#         return render_template('login.html')
+
+# @app.route("/registrate", methods = ['GET', 'POST'])
+# def registration():
+#     if (table_handler.registrate(request.form["createusername"], request.form["createpassword"]) == False):
+#         flash("That username is already taken", "danger")
+#         return render_template('register.html')
+#     else:
+#         flash("Successfully signed up!", "success")
+#         return render_template('login.html')
+        
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
     app.run()
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,10 +1,21 @@
-function fact(n) {
+function fac(n) {
     if (n < 2) {
         return 1;
-    } return (n * fact(n - 1));
+    } return (n * fac(n - 1));
 }
 
 console.log("RETURNING FACT...");
-console.log(fact(1));
-console.log(fact(3));
-console.log(fact(5));
+console.log(fac(1));
+console.log(fac(3));
+console.log(fac(5));
+
+var facB = document.getElementById("facButton");
+
+function runFac() {
+  var facT = document.getElementById("facText");
+  facT = facT.value;
+  document.getElementById("facPara").innerHTML = fac(facT);
+}
+
+facB.addEventListener("click", () => runFac());
+
