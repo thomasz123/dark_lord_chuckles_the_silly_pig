a5408a7a3828f3e2187609d7fe092f33a631623a
Diana Akhmedova <90854530+Dianartemis@users.noreply.github.com>
2023-05-18 12:38:33 -0400

    testing line chart

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,3 +1,27 @@
+const data = {
+  'React': 185134,
+  'Vue': 195514,
+  'Angular': 80460,
+  'Svelte': 57022,
+  'Ember.js': 22165,
+  'Backbone.js': 27862
+};
+
+const ctx = document.getElementById('myChart').getContext('2d');
+
+const myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: Object.keys(data),
+    datasets: [
+      {
+        label: 'Number of GitHub Stars',
+        data: Object.values(data),
+      },
+    ],
+  },
+});
+
 // function fac(n) {
 //     if (n < 2) {
 //         return 1;
@@ -40,35 +64,35 @@
 // }
 
 
-function displayResults() {
-  var files = document.querySelector('#file').files;
+// function displayResults() {
+//   var files = document.querySelector('#file').files;
 
-  if (files.length > 0) {
-    var file = files[0];
-    console.log(file);
+//   if (files.length > 0) {
+//     var file = files[0];
+//     console.log(file);
 
-    var reader = new FileReader(); 
-    reader.readAsText(file);
+//     var reader = new FileReader(); 
+//     reader.readAsText(file);
 
-    reader.onload = function(event) {
-      var csvdata = event.target.result;
-      console.log(csvdata);
+//     reader.onload = function(event) {
+//       var csvdata = event.target.result;
+//       console.log(csvdata);
 
-      var rowData = csvdata.split('\n');
-      var tbodyEl = document.getElementById('tblcsvdata').getElementsByTagName('tbody')[0];
-      tbodyEl.innerHTML = "";
+//       var rowData = csvdata.split('\n');
+//       var tbodyEl = document.getElementById('tblcsvdata').getElementsByTagName('tbody')[0];
+//       tbodyEl.innerHTML = "";
 
-      for (var row = 1; row < rowData.length; row++) {
-        var newRow = tbodyEl.insertRow();
-        rowColData = rowData[row].split(',');
+//       for (var row = 1; row < rowData.length; row++) {
+//         var newRow = tbodyEl.insertRow();
+//         rowColData = rowData[row].split(',');
 
-        for (var col = 0; col < rowColData.length; col++) {
-              var newCell = newRow.insertCell();
-              newCell.innerHTML = rowColData[col];
-        }
-      }
-    };
-  } else {
-    alert("Please select a file.");
-  }
-}
+//         for (var col = 0; col < rowColData.length; col++) {
+//               var newCell = newRow.insertCell();
+//               newCell.innerHTML = rowColData[col];
+//         }
+//       }
+//     };
+//   } else {
+//     alert("Please select a file.");
+//   }
+// }
